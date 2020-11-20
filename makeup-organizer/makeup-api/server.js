8e5775ab735915bd2220906195114823ac2d5f16
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-20 10:41:49 -0500

    initialized mongoose

--- makeup-organizer/makeup-api/server.js
+++ makeup-organizer/makeup-api/server.js
@@ -2,6 +2,25 @@
 const express = require("express");
 const app = express();
 const PORT = 3003;
+// initalize mongoose
+const mongoose = require("mongoose");
+
+// mongoose connection
+// Database Error / Disconnection
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+// Database connection
+mongoose.connect("mongodb://localhost:27017/merncrud", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
 
 // controllers/routes
 const makeupControllers = require("./controllers/makeup.js");
