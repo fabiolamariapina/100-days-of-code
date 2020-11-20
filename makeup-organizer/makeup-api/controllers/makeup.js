9497b5f27f2132b658d778114c7c3c7481d81ddd
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-20 11:33:30 -0500

    created index route

--- makeup-organizer/makeup-api/controllers/makeup.js
+++ makeup-organizer/makeup-api/controllers/makeup.js
@@ -5,13 +5,18 @@ const Makeup = require("../models/makeup.js");
 // ROUTES
 // Index
 makeup.get("/", (req, res) => {
-  res.send("index");
+  Makeup.find({}, (err, foundMakeup) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    res.status(200).json(foundMakeup);
+  });
 });
 // create
 makeup.post("/", async (req, res) => {
   Makeup.create(req.body, (error, createdMakeup) => {
     if (error) {
-      res.status(400), json({ error: error.message });
+      res.status(400).json({ error: error.message });
     }
     res.status(200).send(createdMakeup);
   });
