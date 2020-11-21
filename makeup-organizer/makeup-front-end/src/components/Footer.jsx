9962943d75fb9e6a7b21a589654dff18859aa37a
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-21 18:08:32 -0500

    created footer component

---
+++ makeup-organizer/makeup-front-end/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>2020 All Right Reserved</p>
+        <p>
+          Coded by{" "}
+          <a href="https://www.linkedin.com/in/fabiolampina/">Fabiola Pina</a>
+        </p>
+      </footer>
+    );
+  }
+}
