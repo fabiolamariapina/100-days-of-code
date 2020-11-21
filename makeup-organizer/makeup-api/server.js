556edf0479a3413415d41dca7af1c1f1f61e4379
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-21 17:07:51 -0500

    intialized mongoose

--- makeup-organizer/makeup-api/server.js
+++ makeup-organizer/makeup-api/server.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3003;
+
+// Error / Disconnection
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+//...farther down the page
+
+mongoose.connect("mongodb://localhost:27017/holidays", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
 
 // Controllers/Routes
 const makeupController = require("./controllers/makeup.js");
