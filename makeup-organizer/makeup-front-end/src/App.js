022bab5d3b30af084b5584254e4c21fc445c4e8b
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-21 19:01:28 -0500

    began positioning and changing the sizes of the elements of my landing page

--- makeup-organizer/makeup-front-end/src/App.js
+++ makeup-organizer/makeup-front-end/src/App.js
@@ -7,8 +7,26 @@ export default class App extends Component {
     return (
       <div id="root">
         <Header />
-        <main className="landing-main">
-          <h2 className="call-to-action">I HELP YOU ORGANIZE YOUR MAKEUP</h2>
+        <main>
+          <div className="landing-main">
+            <div className="call-to-action">
+              <h3>I HELP YOU ORGANIZE YOUR MAKEUP</h3>
+            </div>
+            <div className="landing-links">
+              <button className="log-in">
+                <a href="/sessions/new">LOG IN</a>
+              </button>
+              <a className="new-user" href="/users/new">
+                NEW USER? CLICK HERE TO CREATE AN ACCOUNT FOR FREE!
+              </a>
+            </div>
+            <div className="landing-image">
+              <img
+                src="https://www.sephora.com/productimages/sku/s2368454-av-04-zoom.jpg?imwidth=1166"
+                alt="makeup"
+              />
+            </div>
+          </div>
         </main>
         <Footer />
       </div>
