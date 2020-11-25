ac0f92bfcbbc37bc1337ae2f409639658e3c1e61
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-24 19:29:08 -0500

    imported landing page component into App.js

--- makeup-organizer/makeup-front-end/src/App.js
+++ makeup-organizer/makeup-front-end/src/App.js
@@ -1,33 +1,14 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Landing from "./components/Landing";
 
 export default class App extends Component {
   render() {
     return (
       <div id="root">
         <Header />
-        <main>
-          <div className="landing-main">
-            <div className="welcome">
-              <div className="call-to-action">
-                <h3>I HELP YOU ORGANIZE YOUR MAKEUP</h3>
-              </div>
-              <div className="landing-links">
-                <button className="log-in">LOG IN</button>
-                <a className="new-user" href="/users/new">
-                  NEW USER? CLICK HERE TO CREATE AN ACCOUNT FOR FREE!
-                </a>
-              </div>
-            </div>
-            <div className="landing-image">
-              <img
-                src="https://www.sephora.com/productimages/sku/s2368454-av-04-zoom.jpg?imwidth=1166"
-                alt="makeup"
-              />
-            </div>
-          </div>
-        </main>
+        <Landing />
         <Footer />
       </div>
     );
--- makeup-organizer/makeup-front-end/src/components/Landing.jsx
+++ makeup-organizer/makeup-front-end/src/components/Landing.jsx
@@ -1,35 +1,29 @@
 import React, { Component } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
 
 export default class Landing extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <main>
-          <div className="landing-main">
-            <div className="welcome">
-              <div className="call-to-action">
-                <h3>I HELP YOU ORGANIZE YOUR MAKEUP</h3>
-              </div>
-              <div className="landing-links">
-                <button className="log-in">LOG IN</button>
-                <a className="new-user" href="/users/new">
-                  NEW USER? CLICK HERE TO CREATE AN ACCOUNT FOR FREE!
-                </a>
-              </div>
+      <main>
+        <div className="landing-main">
+          <div className="welcome">
+            <div className="call-to-action">
+              <h3>I HELP YOU ORGANIZE YOUR MAKEUP</h3>
             </div>
-            <div className="landing-image">
-              <img
-                src="https://www.sephora.com/productimages/sku/s2368454-av-04-zoom.jpg?imwidth=1166"
-                alt="makeup"
-              />
+            <div className="landing-links">
+              <button className="log-in">LOG IN</button>
+              <a className="new-user" href="/users/new">
+                NEW USER? CLICK HERE TO CREATE AN ACCOUNT FOR FREE!
+              </a>
             </div>
           </div>
-        </main>
-        <Footer />
-      </div>
+          <div className="landing-image">
+            <img
+              src="https://www.sephora.com/productimages/sku/s2368454-av-04-zoom.jpg?imwidth=1166"
+              alt="makeup"
+            />
+          </div>
+        </div>
+      </main>
     );
   }
 }
