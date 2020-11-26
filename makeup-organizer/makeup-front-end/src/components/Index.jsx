70e015116cdb22f59f59f21ead29fb95314cd604
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-25 19:35:15 -0500

    added something to Index.jsx for the time being

---
+++ makeup-organizer/makeup-front-end/src/components/Index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Index extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Your collection</h3>
+      </div>
+    );
+  }
+}
