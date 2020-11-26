a4f3fff03e73e0601c6d25ed799962dcb74dc1b5
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-25 20:10:28 -0500

    fetched data from makeup api

--- makeup-organizer/makeup-front-end/src/App.js
+++ makeup-organizer/makeup-front-end/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+// pages to render
 import Landing from "./components/Landing";
 
 const baseURL = "http://localhost:3003";
@@ -12,6 +13,20 @@ export default class App extends Component {
     this.state = {
       makeup: [],
     };
+    this.getMakeup = this.getMakeup.bind(this);
+  }
+  getMakeup() {
+    fetch(baseURL + "/makeup")
+      .then(
+        (data) => {
+          return data.json();
+        },
+        (err) => console.log(err)
+      )
+      .then(
+        (parsedData) => this.setState({ holidays: parsedData }),
+        (err) => console.log(err)
+      );
   }
   render() {
     return (
