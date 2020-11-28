8692d0c06e373fc980fcf9543d6f7797a9f7ebef
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-28 12:35:00 -0500

    created Edit, New, and Show components

---
+++ makeup-organizer/makeup-front-end/src/components/Show.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Show extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Makeup Product</h3>
+      </div>
+    );
+  }
+}
