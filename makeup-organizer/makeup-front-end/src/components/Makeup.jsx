7165bb402ffc656e76c69855dcea9210b54a781e
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-28 19:58:29 -0500

    passed data through props, data for index page is still not rendering

--- makeup-organizer/makeup-front-end/src/components/Makeup.jsx
+++ makeup-organizer/makeup-front-end/src/components/Makeup.jsx
@@ -5,19 +5,20 @@ export default class Makeup extends Component {
     return (
       <main>
         <h3>Your collection</h3>
-        {/* <div>
+        <div>
           {this.props.makeup.map((makeup) => {
             return (
-              <div>
+              <div key={makeup._id} className="makeup-product">
                 <h4>{makeup.name}</h4>
+                <img>{makeup.img}</img>
+                <div className="edit-delete-buttons">
+                  <button className="edit-button">EDIT</button>
+                  <button className="delete-button">DELETE</button>
+                </div>
               </div>
             );
           })}
-        </div> */}
-        {/* <h4>Name</h4>
-        <p>insert image</p>
-        <p>Edit</p>
-        <p>Delete</p> */}
+        </div>
       </main>
     );
   }
