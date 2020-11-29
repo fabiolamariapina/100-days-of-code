599649f1bf3724b9b6ff183d827bffd64fa0a951
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-29 18:41:48 -0500

    created skeleton of new form component

---
+++ makeup-organizer/makeup-front-end/src/components/NewForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class NewForm extends Component {
+  render() {
+    return (
+      <main>
+        <h3>Testing</h3>
+      </main>
+    );
+  }
+}
