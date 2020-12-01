5e138b519906df43be34954dd1b5bc4eb8b94bcd
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-30 20:56:48 -0500

    set menu to be hidden by default

--- makeup-organizer/makeup-front-end/src/components/DropDown.jsx
+++ makeup-organizer/makeup-front-end/src/components/DropDown.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 
 export default class DropDown extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showMenu: false,
+    };
+  }
   render() {
     return (
       <div className="where-does-product-gp">
         <button>Where does the product go?</button>
-        <div className="drop-down-menu">
-          <button>Eyes</button>
-          <button>Lips</button>
-          <button>Face</button>
-        </div>
+        {this.state.showMenu ? (
+          <div className="drop-down-menu">
+            <button>Eyes</button>
+            <button>Lips</button>
+            <button>Face</button>
+          </div>
+        ) : null}
       </div>
     );
   }
