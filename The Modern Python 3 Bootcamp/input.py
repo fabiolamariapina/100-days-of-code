741191e9879a73aed919b56140839e255db2e558
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-28 17:20:46 -0500

    completed user input code along

---
+++ The Modern Python 3 Bootcamp/input.py
@@ -0,0 +1,10 @@
+# color example
+# color = input("What's you favorite color?")
+# print(f"You said {color}")
+
+# run same code ad above but as a print statement
+# unlike the first bit of code, the input is entered on the next line
+# looks better to me personally
+print("What is your favorite color?")
+color = input()
+print(f"You said {color} ")
