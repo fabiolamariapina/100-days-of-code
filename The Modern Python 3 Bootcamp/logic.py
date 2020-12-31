0ec603258d511f91ec751c51ddac5bc4ec390e4f
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-30 20:44:48 -0500

    multiple elifs code along

---
+++ The Modern Python 3 Bootcamp/logic.py
@@ -0,0 +1,16 @@
+color = input("What is your favorite color?")
+
+if color == "purple":
+    print("excellent choice")
+
+elif color == "teal":
+    print("not bad!")
+
+elif color == "seafoam":
+    print('mediocre')
+
+elif color == "pure darkness":
+    print("I like how you think")
+
+else:
+    print("YOU MONSTER")
