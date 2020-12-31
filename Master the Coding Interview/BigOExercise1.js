6de4877b4f4e97f5b6a6b2c288065c89b89f05d8
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-30 21:02:18 -0500

    took notes on the solution for big O exercise 1

--- Master the Coding Interview/BigOExercise1.js
+++ Master the Coding Interview/BigOExercise1.js
@@ -15,3 +15,21 @@ function funChallenge(input) {
 }
 
 // my guess = 6
+
+// Solution
+function funChallenge(input) {
+  let a = 10; // O(1) --> runs once, defined and consistent
+  a = 50 + 3; // O(1) --> runs once, defined and consistent
+
+  for (let i = 0; i < input.length; i++) {
+    //O(n) -> dependent on input
+    anotherFunction(); // O(n) -> dependnet on input
+    let stranger = true; // O(n) -> dependent on input
+    a++; // O(n) -> dependent on input
+  }
+  return a; // O(1) --> runs once, defined and consistent
+}
+
+// 3 steps that we know of
+// 3 + n + n + n + n
+// solution/official answer: BIG O(3 + 4n)
