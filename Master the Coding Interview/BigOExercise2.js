c002a46ab6adf0216a1f173b49b17c1e75b0c94c
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-30 21:15:07 -0500

    followed along with big O exercise 2 solution video

--- Master the Coding Interview/BigOExercise2.js
+++ Master the Coding Interview/BigOExercise2.js
@@ -17,3 +17,22 @@ function anotherFunChallenge(input) {
 }
 
 // My solution: Big O(4 + 5n)
+
+// Actual Solution per the video
+function anotherFunChallenge(input) {
+  let a = 5; // 0(1)
+  let b = 10; // 0(1)
+  let c = 50; // 0(1)
+  for (let i = 0; i < input; i++) {
+    let x = i + 1; //O(n)
+    let y = i + 2; //O(n)
+    let z = i + 3; //O(n)
+  }
+  for (let j = 0; j < input; j++) {
+    let p = j * 2; //O(n)
+    let q = j * 2; //O(n)
+  }
+  let whoAmI = "I don't know"; // O(1)
+}
+
+//BIG O(4 + 5n) --> IT'S LIT I HAD A VALID SOLUTION!!!
