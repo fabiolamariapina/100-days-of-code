aca668f3fc7a4a7c26b690d93640a3d7e76d6f4c
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-06 18:58:45 -0500

    created header

---
+++ makeup-organizer/makeup-front-end/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <header>
+        <h1 className="app-name">makeup organizer</h1>
+      </header>
+    );
+  }
+}
