27ddc3b3371277f60dc26977fc27d698e5892a5f
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-07 22:05:13 -0500

    began rock, apaper, scissors mini project, decided to tackle it by myself first

---
+++ The Modern Python 3 Bootcamp/basic.py
@@ -0,0 +1,14 @@
+print("rock...")
+print("paper...")
+print("scissors...")
+
+player1 = input("Player 1, make your move: ")
+player2 = input("Player 2, make your move: ")
+
+if player1 and player2:
+    if player1 == "rock" and player2 == "scissors":
+        print("PLAYER 1 WINS")
+    elif player1 == "scissors" and player2 == "rock":
+        print("PLAYER 2 WINS!")
+else:
+    print("Please enter rock, paper, or scissors!")
