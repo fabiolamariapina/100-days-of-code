0e30fda3239014c7aae23eee2c63ae1d43b8f9af
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-09 14:14:18 -0500

    simplified code for winning condition, added code for what happens if there is a tie, and if user types anything other than rock, paper, or scissors

--- The Modern Python 3 Bootcamp/basic.py
+++ The Modern Python 3 Bootcamp/basic.py
@@ -1,22 +1,14 @@
-# print("rock...")
-# print("paper...")
-# print("scissors...")
-
 player1 = input("Player 1, make your move: ")
 player2 = input("Player 2, make your move: ")
 
 if player1 and player2:
-    if player1 == "rock" and player2 == "scissors":
+    if (player1 == "rock" and player2 == "scissors") or (player1 == "scissors" and player2 == "paper") or (player1 == "paper" and player2 == "rock"):
         print("PLAYER 1 WINS")
-    elif player1 == "scissors" and player2 == "rock":
+    elif (player1 == "scissors" and player2 == "rock") or (player1 == "paper" and player2 == "scissors") or (player1 == "rock" and player2 == "paper"):
         print("PLAYER 2 WINS!")
-    elif player1 == "scissors" and player2 == "paper":
-        print("PLAYER 1 WINS!")
-    elif player1 == "paper" and player2 == "scissors":
-        print("PLAYER 2 WINS!")
-    elif player1 == "paper" and player2 == "rock":
-        print("PLAYER 1 WINS!")
-    elif player1 == "rock" and player2 == "paper":
-        print("PLAYER 2 WINS!")
+    elif player1 == player2:
+        print("IT'S A TIE! (insert surprised pikachu here)")
+    else:
+        print("SAY WHAT??")
 else:
     print("Please enter rock, paper, or scissors!")
