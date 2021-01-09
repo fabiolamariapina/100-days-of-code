ecda3f2dc32307e39d3812aabe1bd29551171682
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-09 14:01:42 -0500

    added second scenario where player 2 wins

--- The Modern Python 3 Bootcamp/basic.py
+++ The Modern Python 3 Bootcamp/basic.py
@@ -1,6 +1,6 @@
-print("rock...")
-print("paper...")
-print("scissors...")
+# print("rock...")
+# print("paper...")
+# print("scissors...")
 
 player1 = input("Player 1, make your move: ")
 player2 = input("Player 2, make your move: ")
@@ -12,5 +12,7 @@ if player1 and player2:
         print("PLAYER 2 WINS!")
     elif player1 == "scissors" and player2 == "paper":
         print("PLAYER 1 WINS")
+    elif player1 == "paper" and player2 == "scissors":
+        print("PLAYER 2 WINS")
 else:
     print("Please enter rock, paper, or scissors!")
