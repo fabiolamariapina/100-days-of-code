6fa92fdf5290e7ed495f07dbe2f59badeddc4a17
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-14 19:02:50 -0500

    created footer component

--- makeup-organizer/makeup-front-end/src/App.js
+++ makeup-organizer/makeup-front-end/src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <footer id="footer">
-          <p className="trademark">2020 © All Right Reserved</p>
-          <p className="credit">
-            Coded by{" "}
-            <a
-              className="linkedIn-link"
-              href="https://www.linkedin.com/in/fabiolampina/"
-              target="_blank"
-            >
-              Fabiola Pina
-            </a>
-          </p>
-        </footer>
+        <Footer />
       </div>
     );
   }
