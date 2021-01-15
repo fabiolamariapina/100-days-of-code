aa7ec11971f6a686ca6ea4d45d6282d307070d09
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-14 19:22:28 -0500

    added styling to footer

--- makeup-organizer/makeup-front-end/src/components/Footer.jsx
+++ makeup-organizer/makeup-front-end/src/components/Footer.jsx
@@ -2,12 +2,26 @@ import React, { Component } from "react";
 
 export default class Footer extends Component {
   render() {
+    const footer = {
+      display: "flex",
+      justifyContent: "space-between",
+    };
+    const footerSpiel = {
+      fontStyle: "italic",
+    };
+    const link = {
+      textDecoration: "none",
+      color: "#4f4846",
+    };
     return (
-      <footer id="footer">
-        <p className="trademark">2020 © All Right Reserved</p>
-        <p className="credit">
+      <footer id="footer" style={footer}>
+        <p className="trademark" style={footerSpiel}>
+          2020 © All Right Reserved
+        </p>
+        <p className="credit" style={footerSpiel}>
           Coded by{" "}
           <a
+            style={link}
             className="linkedIn-link"
             href="https://www.linkedin.com/in/fabiolampina/"
             target="_blank"
