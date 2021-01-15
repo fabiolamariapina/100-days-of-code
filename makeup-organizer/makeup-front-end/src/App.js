a4ee13b740b7fd3945af5ef27e6ccfeb84ee7277
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-14 19:01:09 -0500

    created layout for footer

--- makeup-organizer/makeup-front-end/src/App.js
+++ makeup-organizer/makeup-front-end/src/App.js
@@ -6,6 +6,19 @@ export default class App extends Component {
     return (
       <div>
         <Header />
+        <footer id="footer">
+          <p className="trademark">2020 © All Right Reserved</p>
+          <p className="credit">
+            Coded by{" "}
+            <a
+              className="linkedIn-link"
+              href="https://www.linkedin.com/in/fabiolampina/"
+              target="_blank"
+            >
+              Fabiola Pina
+            </a>
+          </p>
+        </footer>
       </div>
     );
   }
