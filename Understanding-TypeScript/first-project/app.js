7bbc5e6e102fb7df128117d5904d3189e287fd3d
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-15 18:50:17 -0500

    coded along to arrays lesson

--- Understanding-TypeScript/first-project/app.js
+++ Understanding-TypeScript/first-project/app.js
@@ -1,7 +1,14 @@
 var person = {
     name: "Fabiola",
-    age: 22
+    age: 22,
+    hobbies: ["Sports", "Cooking"]
 };
 // better to let typescrpt infer when it comes to objects
 // rather than explicitly stating the type of values
+var favoriteActivities;
+favoriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase);
+}
--- Understanding-TypeScript/first-project/app.ts
+++ Understanding-TypeScript/first-project/app.ts
@@ -1,7 +1,16 @@
 const person = {
   name: "Fabiola",
   age: 22,
+  hobbies: ["Sports", "Cooking"],
 };
+
 // better to let typescrpt infer when it comes to objects
 // rather than explicitly stating the type of values
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
