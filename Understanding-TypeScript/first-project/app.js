7a83a61a4e9b344e1907f4a27eb075dabd099d32
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-15 18:58:57 -0500

    coded along to tuples lesson

--- Understanding-TypeScript/first-project/app.js
+++ Understanding-TypeScript/first-project/app.js
@@ -1,14 +1,17 @@
 var person = {
     name: "Fabiola",
     age: 22,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
 // better to let typescrpt infer when it comes to objects
 // rather than explicitly stating the type of values
+person.role.push("admin");
+// person.role[1] = 10;
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby.toUpperCase);
+    console.log(hobby);
 }
--- Understanding-TypeScript/first-project/app.ts
+++ Understanding-TypeScript/first-project/app.ts
@@ -1,11 +1,22 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // tuples- explicity states what types and how many values that array has
+} = {
   name: "Fabiola",
   age: 22,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
 // better to let typescrpt infer when it comes to objects
 // rather than explicitly stating the type of values
+// person.role.push("admin");
+// person.role[1] = 10;
+
+// person.role = [0, "admin"];
+
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
 
