7a70b68f7e87a1968a5b3e770ce4efefcdb90ab0
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-15 19:43:15 -0500

    coded along to literal types lesson

--- Understanding-TypeScript/first-project/app.js
+++ Understanding-TypeScript/first-project/app.js
@@ -1,16 +1,24 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
     // explicitly say that this is where number type goes
-    if (typeof input1 === "number" && typeof input2 == "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     // explicitly say that this is where string type goes
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    //   if (resultConversion === "as-number") {
+    //     return +result;
+    //   } else {
+    //     return result.toString();
+    //   }
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("Spongebob", "Patrick");
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Spongebob", "Patrick", "as-text");
 console.log(combinedNames);
