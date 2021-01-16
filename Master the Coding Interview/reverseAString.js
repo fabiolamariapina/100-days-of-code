875baddea297176693688f3281121a517680d58a
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-16 10:09:22 -0500

    coded along to reverse string solution, like some parts of what I had in my solution better and same applies to instructor solution

--- Master the Coding Interview/reverseAString.js
+++ Master the Coding Interview/reverseAString.js
@@ -7,22 +7,38 @@
 // 2. loop through the array (.forEach())
 // 3. find some way to reverse this
 
-function reverse(str) {
-  // use string split(method) to create array
-  const splitString = str.split(" ");
-  // console.log(splitString);
+// function reverse(str) {
+//   // use string split(method) to create array
+//   const splitString = str.split(" ");
+//   // console.log(splitString);
 
-  // reverse the new array
-  splitString.reverse();
-  // confirm that it reversed sentence
-  // console.log(splitString);
+//   // reverse the new array
+//   splitString.reverse();
+//   // confirm that it reversed sentence
+//   // console.log(splitString);
 
-  // loop through splitString array
-  for (let i = 0; i < splitString.length; i++) {
-    console.log(splitString[i]);
-    // for each word in the array, reverse it
-  }
-}
-reverse("Fabi loves Beyonce and Rihanna");
+//   // loop through splitString array
+//   for (let i = 0; i < splitString.length; i++) {
+//     console.log(splitString[i]);
+//     // for each word in the array, reverse it
+//   }
+// }
+// reverse("Fabi loves Beyonce and Rihanna");
 
 // the actual solution
+// how to ideally appraoch the problem
+// function reverse(str) {
+//   // check input- what if the input is not a string?
+//   if (!str || str.length < 2 || typeof string !== "string") {
+//     return "I do not understand";
+//   }
+//   // convert string into an array
+//   const backwards = [];
+//   const totalItems = str.length - 1;
+//   for (let i = totalItems; i >= 0; i--) {
+//     backwards.push(str[i]);
+//   }
+//   console.log(backwards);
+//   return backwards.join("");
+// }
+// console.log(reverse("Fabi loves Beyonce"));
