079bb1059bcaccc765906f06efc69c9af22d4ac4
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-17 18:34:36 -0500

    gave screaming repeating exercise a try on my own

---
+++ The Modern Python 3 Bootcamp/repeater.py
@@ -0,0 +1,6 @@
+# ask how many times did I have to tell you?
+times = int(input('How many times did I have to tell you?'))
+
+# loop through times
+for number in range(times):
+    print('CLEAN UP YOUR ROOM!')
