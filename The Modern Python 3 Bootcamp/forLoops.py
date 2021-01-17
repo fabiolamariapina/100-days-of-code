a50ec351ddc07863fee6e24e4fd122e1559d688b
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-17 18:13:04 -0500

    coded along to basics of for loops lesson

---
+++ The Modern Python 3 Bootcamp/forLoops.py
@@ -0,0 +1,10 @@
+# EXAMPLE 1
+# FOR LOOP WITH RANGE
+# for x in range(1, 10):
+#     print(x)
+#     print(x*x)
+
+# EXAMPLE 2
+# ITERATE THROUGH A STRING
+for letter in "coffee":
+    print(letter*10)
