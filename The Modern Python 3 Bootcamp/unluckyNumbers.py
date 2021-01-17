d1f98d76ec7481877ec6a23946361d5951e62b3c
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-17 18:46:32 -0500

    tried unlucky numbers exercise by myself

---
+++ The Modern Python 3 Bootcamp/unluckyNumbers.py
@@ -0,0 +1,14 @@
+# loop through numbers 1-20- inclusive
+# for 4 and 13, print "x is unlucky"
+# for even numbers, print "x is even"
+# for odd numbers, print "x is odd"
+
+for x in range(1, 21):
+    # check that it counts 1-20 inclusive
+    # print(x)
+    if (x == 4 or x == 13):
+        print(f"{x} is unlucky")
+    elif (x % 2 == 0):
+        print(f"{x} is even")
+    else:
+        print(f"{x} is odd")
