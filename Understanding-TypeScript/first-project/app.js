cfe0397afe08a226c778aa100b7cbbc0f5ec796d
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-17 17:51:23 -0500

    coded aling to the never type lesson

--- Understanding-TypeScript/first-project/app.js
+++ Understanding-TypeScript/first-project/app.js
@@ -1,38 +1,20 @@
-// Functions
-// start using ES6 syntax
-// let TypeScript infer the type of return
-// Function Return Type Void
-// just means function does not have a return statement
-// it does not return anything
-// undefines can be used if there is a return statement
-// with no value
-var add = function (n1, n2) {
-    return n1 + n2;
+// THE "UNKNOWN" TYPE
+// here, it's unkown because we
+// do not know yet waht the user will
+// eventually answer
+// unlike any, unknown allows for type checking
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Spongebob";
+// extra typecheck to be able to assign
+// an unknown type to a string or any fixed type
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+// THE "NEVER" TYPE
+var generateError = function (message, code) {
+    throw { message: message, erroeCode: code };
 };
-var printResult = function (num) {
-    console.log("Result: " + num);
-};
-printResult(add(5, 12));
-// Function Types and Callbacks
-var addAndHandle = function (n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-};
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
-// Functions as Types
-// Function types allow us to describe which type of
-// function specifically we want to use somewhere
-// in this example, it is made clear that
-// combineValues is a function type
-// A function type is created with ES6 arrow notation
-// after the arrow, specify the retuen type of the function
-// you want to add
-// here, we are telling typescipt that combineValues
-// should accept a function that takes two parameters,
-// where each parameter is a number, and where the
-// function overall then returns a number
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
+var result = generateError("An error ocurred!", 500);
+console.log(result);
--- Understanding-TypeScript/first-project/app.ts
+++ Understanding-TypeScript/first-project/app.ts
@@ -1,45 +1,25 @@
-// Functions
-// start using ES6 syntax
+// THE "UNKNOWN" TYPE
+// here, it's unkown because we
+// do not know yet waht the user will
+// eventually answer
+// unlike any, unknown allows for type checking
+let userInput: unknown;
+let userName: string;
 
-// let TypeScript infer the type of return
+userInput = 5;
+userInput = "Spongebob";
+// extra typecheck to be able to assign
+// an unknown type to a string or any fixed type
+if (typeof userInput === "string") {
+  userName = userInput;
+}
 
-// Function Return Type Void
-// just means function does not have a return statement
-// it does not return anything
-// undefines can be used if there is a return statement
-// with no value
-
-const add = (n1: number, n2: number) => {
-  return n1 + n2;
+// THE "NEVER" TYPE
+// never returns anything
+// esentially crash/break your script
+const generateError = (message: string, code: number): never => {
+  throw { message: message, erroeCode: code };
 };
 
-const printResult = (num: number) => {
-  console.log(`Result: ${num}`);
-};
-
-printResult(add(5, 12));
-
-// Function Types and Callbacks
-const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
-  const result = n1 + n2;
-  cb(result);
-};
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
-
-// Functions as Types
-// Function types allow us to describe which type of
-// function specifically we want to use somewhere
-// in this example, it is made clear that
-// combineValues is a function type
-// A function type is created with ES6 arrow notation
-// after the arrow, specify the retuen type of the function
-// you want to add
-// here, we are telling typescipt that combineValues
-// should accept a function that takes two parameters,
-// where each parameter is a number, and where the
-// function overall then returns a number
-let combineValues: (a: number, b: number) => number;
-combineValues = add;
-console.log(combineValues(8, 8));
+const result = generateError(`An error ocurred!`, 500);
+console.log(result);
