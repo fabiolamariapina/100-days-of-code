efc0a1cfda9af3b626e7004d8fde2c3d01a01297
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-17 19:53:12 -0500

    coded along and took notes on hash tables in different languages lesson

--- Master the Coding Interview/hash.js
+++ Master the Coding Interview/hash.js
@@ -22,3 +22,12 @@ console.log(user.scream()); //O(1)
 // information
 // SOLUTIONS TO HASH COLLISIONS
 // 1. Linked Lists -> later lesson
+
+// MAPS VS OBjects
+// map allows you to save any data type as the
+// key and maintains insertion order
+// objects insert data randomly in different places,
+// not in order
+const a = new Map();
+// similat to objects and maps, sets store data, but exclude keys
+const b = new Sets();
