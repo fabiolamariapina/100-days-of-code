ac3e9c19869653dac8bc79bc727b936fb8b65afa
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-19 15:39:45 -0500

    coded along to core libs lesson

--- Understanding-TypeScript/first-project/app.js
+++ Understanding-TypeScript/first-project/app.js
@@ -1,23 +1,5 @@
 "use strict";
-// THE "UNKNOWN" TYPE
-// here, it's unkown because we
-// do not know yet waht the user will
-// eventually answer
-// unlike any, unknown allows for type checking
-let userInput;
-let userName;
-userInput = 5;
-userInput = "Spongebob";
-// extra typecheck to be able to assign
-// an unknown type to a string or any fixed type
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-// THE "NEVER" TYPE
-// never returns anything
-// esentially crash/break your script
-const generateError = (message, code) => {
-    throw { message: message, erroeCode: code };
-};
-const result = generateError(`An error ocurred!`, 500);
-console.log(result);
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    console.log("CLICK");
+});
