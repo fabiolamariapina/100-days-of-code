0fb6a2fe8ecc31106e0dfe52d9324969889f60cf
fabiolamariapina <fabiolamariapina@gmail.com>
2021-01-21 13:40:04 -0500

    completed stop copying me exercise

---
+++ The Modern Python 3 Bootcamp/stopCopyingMe.py
@@ -0,0 +1,9 @@
+# STOP COPYING ME
+# repeat user input until it equals
+# stop copying me
+message = input("Say something")
+
+while message != "stop copying me":
+    print(message)
+    message = input("Say something: ")
+print("Thank you!")
