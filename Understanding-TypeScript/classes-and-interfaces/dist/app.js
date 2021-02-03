63ea03e99988a0ec14d279dc9dee0da826ad4a3d
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-02 20:55:53 -0500

    learned private and public access modifiers

--- Understanding-TypeScript/classes-and-interfaces/dist/app.js
+++ Understanding-TypeScript/classes-and-interfaces/dist/app.js
@@ -1,15 +1,24 @@
 "use strict";
 var Department = (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log("Department: " + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department("Accounting");
 accounting.describe();
-var accountingCopy = { name: "Dummy", describe: accounting.describe };
-accountingCopy.describe();
+accounting.addEmployee("Bob");
+accounting.addEmployee("Bobina");
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
